5afc7f91f566465ece12f9e20525e07af8f11505
Carter Costic <cartercostic@gmail.com>
2019-06-10 16:33:18 -0400

    Tweaks to the map generator

--- constants.py
+++ constants.py
@@ -7,47 +7,47 @@ cellHeight = 32
 maxMessages = 4
 
 # Map variables
-mapRows = 15
-mapColumns = 25
+mapRows = 30
+mapColumns = 50
 
-# Sprites
-spriteWidth = 32
-# Game Sprites
-forest = pygame.image.load('F:/carter/Pictures/GameAssets/TileFit/forestBox.png')
-path = pygame.image.load('F:/carter/Pictures/GameAssets/TileFit/pathBox.png')
-river = pygame.image.load('F:/carter/Pictures/GameAssets/TileFit/waterBox.png')
-dirt = pygame.image.load('F:/carter/Pictures/GameAssets/TileFit/dirtBox.png')
-# Player Sprites
-wizard = pygame.image.load('F:/carter/Pictures/GameAssets/TileFit/wizardBox.png')
-warrior = pygame.image.load('F:/carter/Pictures/GameAssets/TileFit/warriorBox.png')
-assassin = pygame.image.load('F:/carter/Pictures/GameAssets/TileFit/assassinBox.png')
-archer = pygame.image.load('F:/carter/Pictures/GameAssets/TileFit/archerBox.png')
-# Enemy Sprites
-skeleton = pygame.image.load('F:/carter/Pictures/GameAssets/TileFit/skeletonBox.png')
-squirrel = pygame.image.load('F:/carter/Pictures/GameAssets/TileFit/squirrelBox.png')
-ninja = pygame.image.load('F:/carter/Pictures/GameAssets/TileFit/ninjaBox.png')
-dragonPassive = pygame.image.load('F:/carter/Pictures/GameAssets/TileFit/dragonPassiveBox.png')
-dragonAggressive = pygame.image.load('F:/carter/Pictures/GameAssets/TileFit/dragonAggressiveBox.png')
-darkWarrior = pygame.image.load('F:/carter/Pictures/GameAssets/TileFit/darkwarriorBox.png')
-
-# Colors
-defaultColor = (100, 100, 100)
-
-
-# Main Menu
-displaySize = (mapColumns * cellWidth, mapRows * cellHeight)
-menuBackground = pygame.image.load('F:/carter/Pictures/GameAssets/Background/menuBackground.jpg')
-menuButtonWidth = 150
-menuButtonHeight = 50
-menuButtonColorLight = (214, 237, 255)
-menuButtonColorDark = (179, 223, 255)
-menuButtonFont = "Calibri"
-menuButtonFontSize = 30
-
-# Character Selection Menu
-characterImageSize = (150, 150)
-# Raw Player Sprites
-wizardRaw = pygame.image.load('F:/carter/Pictures/GameAssets/RawImages/wizard.png')
-# warriorRaw
-archerRaw = pygame.image.load('F:/carter/Pictures/GameAssets/RawImages/archer.png')
-# assassinRaw =
+# # Sprites
+# spriteWidth = 32
+# # Game Sprites
+# forest = pygame.image.load('F:/carter/Pictures/GameAssets/TileFit/forestBox.png')
+# path = pygame.image.load('F:/carter/Pictures/GameAssets/TileFit/pathBox.png')
+# river = pygame.image.load('F:/carter/Pictures/GameAssets/TileFit/waterBox.png')
+# dirt = pygame.image.load('F:/carter/Pictures/GameAssets/TileFit/dirtBox.png')
+# # Player Sprites
+# wizard = pygame.image.load('F:/carter/Pictures/GameAssets/TileFit/wizardBox.png')
+# warrior = pygame.image.load('F:/carter/Pictures/GameAssets/TileFit/warriorBox.png')
+# assassin = pygame.image.load('F:/carter/Pictures/GameAssets/TileFit/assassinBox.png')
+# archer = pygame.image.load('F:/carter/Pictures/GameAssets/TileFit/archerBox.png')
+# # Enemy Sprites
+# skeleton = pygame.image.load('F:/carter/Pictures/GameAssets/TileFit/skeletonBox.png')
+# squirrel = pygame.image.load('F:/carter/Pictures/GameAssets/TileFit/squirrelBox.png')
+# ninja = pygame.image.load('F:/carter/Pictures/GameAssets/TileFit/ninjaBox.png')
+# dragonPassive = pygame.image.load('F:/carter/Pictures/GameAssets/TileFit/dragonPassiveBox.png')
+# dragonAggressive = pygame.image.load('F:/carter/Pictures/GameAssets/TileFit/dragonAggressiveBox.png')
+# darkWarrior = pygame.image.load('F:/carter/Pictures/GameAssets/TileFit/darkwarriorBox.png')
+#
+# # Colors
+# defaultColor = (100, 100, 100)
+#
+#
+# # Main Menu
+# displaySize = (mapColumns * cellWidth, mapRows * cellHeight)
+# menuBackground = pygame.image.load('F:/carter/Pictures/GameAssets/Background/menuBackground.jpg')
+# menuButtonWidth = 150
+# menuButtonHeight = 50
+# menuButtonColorLight = (214, 237, 255)
+# menuButtonColorDark = (179, 223, 255)
+# menuButtonFont = "Calibri"
+# menuButtonFontSize = 30
+#
+# # Character Selection Menu
+# characterImageSize = (150, 150)
+# # Raw Player Sprites
+# wizardRaw = pygame.image.load('F:/carter/Pictures/GameAssets/RawImages/wizard.png')
+# # warriorRaw
+# archerRaw = pygame.image.load('F:/carter/Pictures/GameAssets/RawImages/archer.png')
+# # assassinRaw =
